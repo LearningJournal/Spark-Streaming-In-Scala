3e8d15a07bbd229fea8c1ca5a6538563a45ae409
Learning Journal <pandey.kumar.prashant@gmail.com>
2020-07-26 23:32:52 +0530

    Initial Commit

---
+++ 03-KafkaStreamDemo/build.sbt
@@ -0,0 +1,14 @@
+name := "KafkaStreamDemo"
+organization := "guru.learningjournal"
+version := "0.1"
+scalaVersion := "2.12.10"
+autoScalaLibrary := false
+val sparkVersion = "3.0.0"
+
+val sparkDependencies = Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "org.apache.spark" %% "spark-sql-kafka-0-10" % sparkVersion
+)
+
+libraryDependencies ++= sparkDependencies
---
+++ 03-KafkaStreamDemo/src/main/scala/KafkaStreamDemo.scala
@@ -0,0 +1,87 @@
+import org.apache.log4j.Logger
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.sql.types.{ArrayType, DoubleType, IntegerType, LongType, StringType, StructField, StructType}
+
+object KafkaStreamDemo extends Serializable {
+  @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder()
+      .master("local[3]")
+      .appName("Kafka Stream Demo")
+      .config("spark.streaming.stopGracefullyOnShutdown", "true")
+      .getOrCreate()
+
+    val schema = StructType(List(
+      StructField("InvoiceNumber", StringType),
+      StructField("CreatedTime", LongType),
+      StructField("StoreID", StringType),
+      StructField("PosID", StringType),
+      StructField("CashierID", StringType),
+      StructField("CustomerType", StringType),
+      StructField("CustomerCardNo", StringType),
+      StructField("TotalAmount", DoubleType),
+      StructField("NumberOfItems", IntegerType),
+      StructField("PaymentMethod", StringType),
+      StructField("CGST", DoubleType),
+      StructField("SGST", DoubleType),
+      StructField("CESS", DoubleType),
+      StructField("DeliveryType", StringType),
+      StructField("DeliveryAddress", StructType(List(
+        StructField("AddressLine", StringType),
+        StructField("City", StringType),
+        StructField("State", StringType),
+        StructField("PinCode", StringType),
+        StructField("ContactNumber", StringType)
+      ))),
+      StructField("InvoiceLineItems", ArrayType(StructType(List(
+        StructField("ItemCode", StringType),
+        StructField("ItemDescription", StringType),
+        StructField("ItemPrice", DoubleType),
+        StructField("ItemQty", IntegerType),
+        StructField("TotalValue", DoubleType)
+      )))),
+    ))
+
+    val kafkaDF = spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "invoices")
+      .option("startingOffsets", "earliest")
+      .load()
+
+    //kafkaDF.printSchema()
+
+    val valueDF = kafkaDF.select(from_json(col("value").cast("string"), schema).alias("value"))
+
+    val explodeDF = valueDF.selectExpr("value.InvoiceNumber", "value.CreatedTime", "value.StoreID",
+      "value.PosID", "value.CustomerType", "value.PaymentMethod", "value.DeliveryType", "value.DeliveryAddress.City",
+      "value.DeliveryAddress.State", "value.DeliveryAddress.PinCode", "explode(value.InvoiceLineItems) as LineItem")
+
+    val flattenedDF = explodeDF
+      .withColumn("ItemCode", expr("LineItem.ItemCode"))
+      .withColumn("ItemDescription", expr("LineItem.ItemDescription"))
+      .withColumn("ItemPrice", expr("LineItem.ItemPrice"))
+      .withColumn("ItemQty", expr("LineItem.ItemQty"))
+      .withColumn("TotalValue", expr("LineItem.TotalValue"))
+      .drop("LineItem")
+
+    val invoiceWriterQuery = explodeDF.writeStream
+      .format("json")
+      .queryName("Flattened Invoice Writer")
+      .outputMode("append")
+      .option("path", "output")
+      .option("checkpointLocation", "chk-point-dir")
+      .trigger(Trigger.ProcessingTime("1 minute"))
+      .start()
+
+    logger.info("Listening to Kafka")
+    invoiceWriterQuery.awaitTermination()
+
+  }
+
+}
