2c0717f91fb30ce6cc87e6c8c324aa57561618bc
Learning Journal <pandey.kumar.prashant@gmail.com>
2020-07-29 16:46:33 +0530

    Initial Commit

---
+++ 04-KafkaSinkDemo/src/main/scala/KafkaSinkDemo.scala
@@ -0,0 +1,94 @@
+import org.apache.log4j.Logger
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, expr, from_json}
+import org.apache.spark.sql.types.{ArrayType, DoubleType, IntegerType, LongType, StringType, StructField, StructType}
+
+object KafkaSinkDemo extends Serializable {
+  @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder()
+      .master("local[3]")
+      .appName("Kafka Sink Demo")
+      .config("spark.streaming.stopGracefullyOnShutdown", "true")
+      .getOrCreate()
+
+    val schema = StructType(List(
+      StructField("InvoiceNumber", StringType),
+      StructField("CreatedTime", LongType),
+      StructField("StoreID", StringType),
+      StructField("PosID", StringType),
+      StructField("CashierID", StringType),
+      StructField("CustomerType", StringType),
+      StructField("CustomerCardNo", StringType),
+      StructField("TotalAmount", DoubleType),
+      StructField("NumberOfItems", IntegerType),
+      StructField("PaymentMethod", StringType),
+      StructField("CGST", DoubleType),
+      StructField("SGST", DoubleType),
+      StructField("CESS", DoubleType),
+      StructField("DeliveryType", StringType),
+      StructField("DeliveryAddress", StructType(List(
+        StructField("AddressLine", StringType),
+        StructField("City", StringType),
+        StructField("State", StringType),
+        StructField("PinCode", StringType),
+        StructField("ContactNumber", StringType)
+      ))),
+      StructField("InvoiceLineItems", ArrayType(StructType(List(
+        StructField("ItemCode", StringType),
+        StructField("ItemDescription", StringType),
+        StructField("ItemPrice", DoubleType),
+        StructField("ItemQty", IntegerType),
+        StructField("TotalValue", DoubleType)
+      )))),
+    ))
+
+    val kafkaSourceDF = spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "invoices")
+      .option("startingOffsets", "earliest")
+      .load()
+
+    val valueDF = kafkaSourceDF.select(from_json(col("value").cast("string"), schema).alias("value"))
+
+    val notificationDF = valueDF.select("value.InvoiceNumber", "value.CustomerCardNo", "value.TotalAmount")
+      .withColumn("EarnedLoyaltyPoints", expr("TotalAmount * 0.2"))
+
+    //val kafkaTargetDF = notificationDF.selectExpr("InvoiceNumber as key", "to_json(struct(*)) as value")
+
+    val kafkaTargetDF = notificationDF.selectExpr("InvoiceNumber as key",
+      """to_json(named_struct('CustomerCardNo', CustomerCardNo,
+        |'TotalAmount', TotalAmount,
+        |'EarnedLoyaltyPoints', TotalAmount * 0.2
+        |)) as value""".stripMargin)
+
+    /*
+    val notificationWriterQuery = kafkaTargetDF.writeStream
+      .format("console")
+      .outputMode("append")
+      .option("truncate", "false")
+      .option("checkpointLocation", "chk-point-dir")
+      .start()
+    */
+
+
+    val notificationWriterQuery = kafkaTargetDF
+      .writeStream
+      .queryName("Notification Writer")
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("topic", "notifications")
+      .outputMode("append")
+      .option("checkpointLocation", "chk-point-dir")
+      .start()
+
+    logger.info("Listening and writing to Kafka")
+    notificationWriterQuery.awaitTermination()
+
+  }
+
+}
