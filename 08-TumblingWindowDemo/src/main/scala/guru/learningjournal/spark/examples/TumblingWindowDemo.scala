2eba1d559ba0046f795889f8d34171bbe0b7b1c2
Prashant Kumar Pandey <LearningJournal@users.noreply.github.com>
2020-08-17 21:53:29 +0530

    Initial Commit

--- 08-TumblingWindowDemo/src/main/scala/guru/learningjournal/spark/examples/TumblingWindowDemo.scala
+++ 08-TumblingWindowDemo/src/main/scala/guru/learningjournal/spark/examples/TumblingWindowDemo.scala
@@ -4,7 +4,7 @@ import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.Trigger
-import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructType}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 
 object TumblingWindowDemo extends Serializable {
   @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
@@ -15,39 +15,43 @@ object TumblingWindowDemo extends Serializable {
       .master("local[3]")
       .appName("Tumbling Window Demo")
       .config("spark.streaming.stopGracefullyOnShutdown", "true")
+      .config("spark.sql.shuffle.partitions", 2)
       .getOrCreate()
 
-    val invoiceSchema = StructType(List(
-      StructField("InvoiceNumber", StringType),
+    val stockSchema = StructType(List(
       StructField("CreatedTime", StringType),
-      StructField("StoreID", StringType),
-      StructField("TotalAmount", DoubleType)
+      StructField("Type", StringType),
+      StructField("Amount", IntegerType),
+      StructField("BrokerCode", StringType)
     ))
 
     val kafkaSourceDF = spark
       .readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
-      .option("subscribe", "invoices")
+      .option("subscribe", "trades")
       .option("startingOffsets", "earliest")
       .load()
 
-    val valueDF = kafkaSourceDF.select(from_json(col("value").cast("string"), invoiceSchema).alias("value"))
+    val valueDF = kafkaSourceDF.select(from_json(col("value").cast("string"), stockSchema).alias("value"))
 
-    //valueDF.printSchema()
-    //valueDF.show(false)
-
-    val invoiceDF = valueDF.select("value.*")
+    val tradeDF = valueDF.select("value.*")
       .withColumn("CreatedTime", to_timestamp(col("CreatedTime"), "yyyy-MM-dd HH:mm:ss"))
+      .withColumn("Buy", expr("case when Type == 'BUY' then Amount else 0 end"))
+      .withColumn("Sell", expr("case when Type == 'SELL' then Amount else 0 end"))
 
-    val countDF = invoiceDF.groupBy(col("StoreID"),
-      window(col("CreatedTime"), "5 minute"))
-      .count()
+    //tradeDF.printSchema()
+    //tradeDF.show(false)
 
-    //countDF.printSchema()
-    //countDF.show(false)
+    val windowAggDF = tradeDF
+      .groupBy( // col("BrokerCode"),
+        window(col("CreatedTime"), "15 minute"))
+      .agg(sum("Buy").alias("TotalBuy"),
+        sum("Sell").alias("TotalSell"))
 
-    val outputDF = countDF.select("StoreID", "window.start", "window.end", "count")
+    val outputDF = windowAggDF.select("window.start", "window.end", "TotalBuy", "TotalSell")
+
+    //outputDF.show(false)
 
     val windowQuery = outputDF.writeStream
       .format("console")
